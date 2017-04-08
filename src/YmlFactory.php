308154d8c73a185bb81dcc222ebd2c8089c078af
DartVadius <vad261275@gmail.com>
2017-04-08 15:00:03 +0300

    v0.2

--- src/YmlFactory.php
+++ src/YmlFactory.php
@@ -21,11 +21,27 @@ use DartVadius\YmlGenerator\offer\VendorYmlOffer;
  */
 class YmlFactory {
     public static function getYml($name) {
-        $class = ucfirst($name) . 'YmlOffer';        
-        if (class_exists($class)) {
-            return new $class();
-        } else {
-            throw new Exception("Wrong class");
+        switch (strtolower($name)) {
+            case 'head':                
+                return new HeadYmlOffer();
+            case 'bookaudio':
+                return new BookAudioYmlOffer();
+            case 'bookpaper':
+                return new BookPaperYmlOffer();
+            case 'event':
+                return new EventYmlOffer();
+            case 'media':
+                return new MediaYmlOffer();
+            case 'medicine':
+                return new MedicineYmlOffer();
+            case 'simple':
+                return new SimpleYmlOffer();
+            case 'tour':
+                return new TourYmlOffer();
+            case 'vendor':
+                return new VendorYmlOffer();    
+            default :
+                return FALSE;
         }
     }
 }
