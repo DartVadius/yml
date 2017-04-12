a5c451209220acc14ca933990533e6992aa1ab76
DartVadius <vad261275@gmail.com>
2017-04-12 16:20:43 +0300

    v0.9

--- src/YmlFactory.php
+++ src/YmlFactory.php
@@ -2,36 +2,22 @@
 
 namespace DartVadius\YmlGenerator;
 
-use DartVadius\YmlGenerator\offer;
-
 /**
  * YmlFactory
  *
  * @author DartVadius
  */
 class YmlFactory {
+
     public static function getYml($name) {
-        switch (strtolower($name)) {
-            case 'head':                
-                return new offer\HeadYmlOffer();
-            case 'bookaudio':
-                return new offer\BookAudioYmlOffer();
-            case 'bookpaper':
-                return new offer\BookPaperYmlOffer();
-            case 'event':
-                return new offer\EventYmlOffer();
-            case 'media':
-                return new offer\MediaYmlOffer();
-            case 'medicine':
-                return new offer\MedicineYmlOffer();
-            case 'simple':
-                return new offer\SimpleYmlOffer();
-            case 'tour':
-                return new offer\TourYmlOffer();
-            case 'vendor':
-                return new offer\VendorYmlOffer();    
-            default :
-                return FALSE;
+        $pathToClassFile = __DIR__ . '/offer/' . ucfirst($name) . 'YmlOffer.php';
+        if (file_exists($pathToClassFile)) {
+            require_once $pathToClassFile;
         }
+        if (class_exists(ucfirst($name) . 'YmlOffer')) {
+            return new ucfirst($name) . 'YmlOffer';
+        }
+        return FALSE;        
     }
+
 }
